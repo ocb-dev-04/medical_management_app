0328dce43a04059df577157c4360dca5d844cec6
Oscar Chavez <42700726+ocb-dev-04@users.noreply.github.com>
2024-12-22 16:33:16 -0400

    *add search from elastic instead relational database

--- src/services/doctor/Services.Doctors.Application/UseCases/GetCollectionByName/GetDoctorCollectionByNameQueryHandler.cs
+++ src/services/doctor/Services.Doctors.Application/UseCases/GetCollectionByName/GetDoctorCollectionByNameQueryHandler.cs
@@ -1,8 +1,6 @@
 ﻿using Services.Doctors.Domain.Dtos;
 using Shared.Common.Helper.ErrorsHandler;
 using Shared.Domain.Abstractions.Services;
-using Services.Doctors.Domain.Abstractions;
-using Value.Objects.Helper.Values.Primitives;
 using CQRS.MediatR.Helper.Abstractions.Messaging;
 using Shared.Domain.Constants;
 
@@ -11,17 +9,13 @@ namespace Services.Doctors.Application.UseCases;
 internal sealed class GetDoctorCollectionByNameQueryHandler
     : IQueryHandler<GetDoctorCollectionByNameQuery, IEnumerable<DoctorDto>>
 {
-    private readonly IDoctorRepository _doctorRepository;
     private readonly IElasticSearchService<DoctorDto> _doctorSearchClient;
 
     public GetDoctorCollectionByNameQueryHandler(
-        IDoctorRepository doctorRepository, 
         IElasticSearchService<DoctorDto> doctorSearchClient)
     {
-        ArgumentNullException.ThrowIfNull(doctorRepository, nameof(doctorRepository));
         ArgumentNullException.ThrowIfNull(doctorSearchClient, nameof(doctorSearchClient));
 
-        _doctorRepository = doctorRepository;
         _doctorSearchClient = doctorSearchClient;
     }
 
@@ -32,11 +26,6 @@ internal sealed class GetDoctorCollectionByNameQueryHandler
             request.Name,
             ServicesConstants.ElasticSearchDoctorIndex,
             cancellationToken);
-
-        //IReadOnlyCollection<DoctorDto> collection = await _doctorRepository.CollectionByNameAsync(
-        //    StringObject.Create(request.Name), 
-        //    request.PageNumber, 
-        //    cancellationToken);
 
         if (collection.IsFailure)
             return Result.Failure<IEnumerable<DoctorDto>>(collection.Error);

f8431191c4819f23f3e39877ffd25f76d1cdda31
Oscar Chavez <42700726+ocb-dev-04@users.noreply.github.com>
2025-01-13 18:44:06 -0400

    *Add code to RemoveDiagnosis handler tests

--- src/tests/Services.Diagnoses.Application.UnitTests/BaseTestSharedConfiguration.cs
+++ src/tests/Services.Diagnoses.Application.UnitTests/BaseTestSharedConfiguration.cs
@@ -4,6 +4,9 @@ using Service.Diagnoses.Domain.Entities;
 using Shared.Common.Helper.ErrorsHandler;
 using Service.Diagnoses.Domain.Abstractions;
 using Shared.Message.Queue.Requests.Responses;
+using Service.Diagnoses.Domain.Enums;
+using Value.Objects.Helper.Values.Primitives;
+using Service.Diagnoses.Domain.Errors;
 
 namespace Services.Diagnoses.Application.UnitTests;
 
@@ -16,6 +19,8 @@ public abstract class BaseTestSharedConfiguration
 
     protected readonly DoctorQueueResponse _validDoctor;
     protected readonly PatientQueueResponse _validPatient;
+    protected readonly Diagnosis _validDiagnosis;
+    protected readonly Diagnosis _validDiagnosisWithOtherOwner;
 
     protected BaseTestSharedConfiguration()
     {
@@ -35,13 +40,29 @@ public abstract class BaseTestSharedConfiguration
             _faker.Person.FullName,
             _faker.Random.Number(80),
             DateTimeOffset.UtcNow);
+
+        _validDiagnosis = Diagnosis.Create(
+            GuidObject.Create(_validDoctor.Id.ToString()),
+            GuidObject.Create(_validPatient.Id.ToString()),
+            StringObject.Create(_faker.Commerce.ProductName()),
+            StringObject.Create(_faker.Commerce.ProductDescription()),
+            StringObject.Create(_faker.Lorem.Paragraph(240)),
+            DosageIntervals.EverySixHours.ToTimeSpan());
+
+        _validDiagnosisWithOtherOwner = Diagnosis.Create(
+            GuidObject.New(),
+            GuidObject.Create(_validPatient.Id.ToString()),
+            StringObject.Create(_faker.Commerce.ProductName()),
+            StringObject.Create(_faker.Commerce.ProductDescription()),
+            StringObject.Create(_faker.Lorem.Paragraph(240)),
+            DosageIntervals.EverySixHours.ToTimeSpan());
     }
 
     #region Doctor Message Queue
 
     public void Set_DoctorMessageQueue_Succes()
         => _messageQeueServicesMock.GetDoctorByIdAsync(
-                        Arg.Any<Guid>(),
+                        _validDoctor.Id,
                         Arg.Any<CancellationToken>())
                     .Returns(_validDoctor);
 
@@ -67,7 +88,7 @@ public abstract class BaseTestSharedConfiguration
 
     public void Set_PatientMessageQueue_Succes()
         => _messageQeueServicesMock.GetPatientByIdAsync(
-                        Arg.Any<Guid>(),
+                        _validPatient.Id,
                         Arg.Any<CancellationToken>())
                     .Returns(_validPatient);
 
@@ -90,7 +111,28 @@ public abstract class BaseTestSharedConfiguration
 
     #region Diagnoses Repository
     
-    public void Set_CreateDiagnoses_Success()
+    public void Set_GetDiagnosisById_Success()
+        => _diagnosisRepositoryMock.ByIdAsync(
+                        _validDiagnosis.Id,
+                        Arg.Any<CancellationToken>())
+                    .Returns(_validDiagnosis);
+
+    public void Set_GetDiagnosisById_Success_WithOtherOwner()
+        => _diagnosisRepositoryMock.ByIdAsync(
+                        Arg.Any<GuidObject>(),
+                        //_validDiagnosisWithOtherOwner.Id,
+                        Arg.Any<CancellationToken>())
+                    .Returns(_validDiagnosisWithOtherOwner);
+
+    public void Set_GetDiagnosisById_NotFound()
+        => _diagnosisRepositoryMock.ByIdAsync(
+                        Arg.Any<GuidObject>(),
+                        Arg.Any<CancellationToken>())
+                    .Returns(
+                        Result.Failure<Diagnosis>(
+                            DiagnosisErrors.NotFound));
+    
+    public void Set_CreateDiagnosis_Success()
         => _diagnosisRepositoryMock.CreateAsync(
                         Arg.Any<Diagnosis>(),
                         Arg.Any<CancellationToken>())
--- src/tests/Services.Diagnoses.Application.UnitTests/UseCases/Remove/RemoveDiagnosisCommandHandlerTest.cs
+++ src/tests/Services.Diagnoses.Application.UnitTests/UseCases/Remove/RemoveDiagnosisCommandHandlerTest.cs
@@ -1,4 +1,9 @@
-﻿using Service.Diagnoses.Application.UseCases;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using Service.Diagnoses.Application.UseCases;
+using Shared.Common.Helper.ErrorsHandler;
+using Value.Objects.Helper.Values.Primitives;
 
 namespace Services.Diagnoses.Application.UnitTests.UseCases.Remove;
 
@@ -10,9 +15,101 @@ public sealed class RemoveDiagnosisCommandHandlerTest
 
     public RemoveDiagnosisCommandHandlerTest()
     {
-        _command = new(_validPatient.Id, _validDoctor.Id);
+        _command = new(_validDiagnosis.Id.Value, _validDoctor.Id);
         _handler = new(
-            _diagnosisRepositoryMock, 
+            _diagnosisRepositoryMock,
             _messageQeueServicesMock);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnSuccessResult()
+    {
+        // arrange
+        Set_DoctorMessageQueue_Succes();
+        Set_GetDiagnosisById_Success();
+
+        // act
+        Result result = await _handler.Handle(_command, default);
+
+        // assert
+        await _messageQeueServicesMock.Received(1)
+            .GetDoctorByIdAsync(Arg.Is<Guid>(f
+                => f.Equals(_command.DoctorId)), default);
+
+        await _diagnosisRepositoryMock.Received(1)
+            .ByIdAsync(Arg.Is<GuidObject>(f
+                => f.Value.Equals(_command.Id)), default);
+
+        await _diagnosisRepositoryMock.Received(1)
+            .DeleteAsync(Arg.Is<GuidObject>(f
+                => f.Equals(_validDiagnosis.Id)), default);
+
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailedResult_DoctorNotFound()
+    {
+        // arrange
+        Set_DoctorMessageQueue_NotFoundFailure();
+
+        // act
+        Result result = await _handler.Handle(_command, default);
+
+        // assert
+        await _messageQeueServicesMock.Received(1)
+            .GetDoctorByIdAsync(Arg.Is<Guid>(f
+                => f.Equals(_command.DoctorId)), default);
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailedResult_DoctorNullValue()
+    {
+        // arrange
+        Set_DoctorMessageQueue_NullValueFailure();
+
+        // act
+        Result result = await _handler.Handle(_command, default);
+
+        // assert
+        await _messageQeueServicesMock.Received(1)
+            .GetDoctorByIdAsync(Arg.Is<Guid>(f
+                => f.Equals(_command.DoctorId)), default);
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(Error.NullValue);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailedResult_DiagnosisByIdNotFound()
+    {
+        // arrange
+        Set_DoctorMessageQueue_Succes();
+        Set_GetDiagnosisById_NotFound();
+
+        // act
+        Result result = await _handler.Handle(_command, default);
+
+        // assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailedResult_DiagnosisDoctorIdIsNotTheOwner()
+    {
+        // arrange
+        Set_DoctorMessageQueue_Succes();
+        Set_GetDiagnosisById_Success_WithOtherOwner();
+
+        // act
+        Result result = await _handler.Handle(_command, default);
+
+        // assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
     }
 }
